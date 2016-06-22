8fc3d112f7c0a39ba25711457d4bb5b6a6ee93a4
Oleg Stepura <github@oleg.stepura.com>
2016-06-22 17:02:19 +0200

    Test for correct CSS modules generated annotations import

--- src/main/some/name/space/Test1.tsx
+++ src/main/some/name/space/Test1.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Test2 } from 'some/other/ns/Test2'
+import * as style from './Style.css'
 
 export class Test1 extends Component<{}, {}> {
   render() {
